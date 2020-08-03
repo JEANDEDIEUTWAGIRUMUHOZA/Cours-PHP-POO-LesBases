b1ae590f138851251cc6e19303613265d34e61c9
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-08-03 08:36:55 +0200

    notion de constructeur et d'instation

--- intro.php
+++ intro.php
@@ -9,23 +9,42 @@
   Une clesse est une moule pour créer nos objets
 *Sécurité du code avec l'encapsulation
 *Réutilsation du code avec l'héritage
-*Controle avec les classes abstraites et les interfaces
+*Avoir plus de Controle avec les classes abstraites et les interfaces
 *Enfin l'abstration et polymorphisme
 
  */
 
   class Employe{
 
-   public $nom;
-   public $prenom;
-   public  $age;
+           public $nom;
+           public $prenom;
+           public  $age;
 
-   function presentation($nom,$prenom,$age){
-       echo "Je suis ".$prenom."".$nom." et j'ai ".$age. "ans";
 
-   }
+           /*constructeur pour initialiser les attribut d'une instance
+             à chaque fois que l'on vaa créer une nouvelle instance cet objet, on aura besoin 
+             de la passer les attribut de cette classe
+           */
 
-      }
+           public function __construct($nom,$prenom,$age){
+               $this->$nom =  $nom; //égale nom veut dire que le nom sera celui fournier lors de el'instanciation
+               $this->$prenom = $prenom;
+               $this->$age = $age;
+           }
 
+           function presentation($nom,$prenom,$age){
+              echo "Je suis ".$this->$nom."".$this->$prenom." et j'ai ".$this->$age. " ans";
+
+            }
+
+          
+
+           // presentation("Jean","Dupond",54);
+       
+      
+        }
+
+        $employe1 = new Employe("Jean ", "Durant", 45);
+        $employe1->presentation("Jean ", "Durant", 45);
    ?>
 
---
+++ objets.php
@@ -0,0 +1,14 @@
+<?php
+
+/*require('intro.php');
+
+$employe1  = new Employe();
+$employe1->nom  = "Jean de Dieu";
+$employe1->prenom = "TWAGIRUMUHOZA";
+$employe1->age =  28;
+
+//appel de la fonction sur l'employé1
+
+prensentation($employe1->nom,$employe1->prenom,$employe->age);*/
+
+?>
